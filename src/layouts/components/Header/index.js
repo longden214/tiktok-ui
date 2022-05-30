d363130b837a80cf5493bc9672d4b551407b2076
longden <nhokimlien1411@gmail.com>
2022-05-30 11:44:17 +0700

    Tái Cấu Trúc Và Tối Ưu Code #1

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -14,10 +14,10 @@ import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 import { Link } from 'react-router-dom';
 
-import routesConfig from '~/config/routes';
+import config from '~/config';
 import Button from '~/components/Button';
 import Menu from '~/components/Popper/Menu';
-import Search from '~/components/Layout/components/Search';
+import Search from '~/layouts/components/Search';
 import { InboxIcon, MessageIcon, UploadIcon } from '~/components/Icons';
 import Image from '~/components/Image';
 
@@ -99,7 +99,7 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <Link to={routesConfig.home} className={cx('logo-link')}>
+                <Link to={config.routes.home} className={cx('logo-link')}>
                     <img src={images.logo} alt="Tiktok" />
                 </Link>
                 <Search />
